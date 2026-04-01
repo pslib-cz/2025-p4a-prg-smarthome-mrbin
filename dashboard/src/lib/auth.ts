25ce18e3ea55d7a441ce555adecb09906a6c3eb8
Martin Holý <115696258+MartinHoly00@users.noreply.github.com>
2026-04-01 13:19:28 +0200

    fix

--- dashboard/src/lib/auth.ts
+++ dashboard/src/lib/auth.ts
@@ -26,4 +26,17 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
   session: {
     strategy: "jwt",
   },
+  callbacks: {
+    authorized({ auth, request: { nextUrl } }) {
+      const isLoggedIn = !!auth?.user;
+      const isOnLogin = nextUrl.pathname === "/login";
+
+      if (isOnLogin) {
+        if (isLoggedIn) return Response.redirect(new URL("/", nextUrl));
+        return true;
+      }
+
+      return isLoggedIn;
+    },
+  },
 });
